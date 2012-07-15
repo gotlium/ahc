0506785bb2f4d0bee971ea01d9fc70495448a606
gotlium <death-metall@mail.ru>
2012-07-15 15:52:01 +0500

    Git deployment.

--- modules/git.py
+++ modules/git.py
@@ -1,8 +1,9 @@
 __author__ = 'gotlium'
 
+from libraries.path import HostPath
 from libraries.helpers import *
 
-class Git(object):
+class Git(HostPath):
 
 	methods = ('add', 'delete',)
 	base = None
@@ -13,13 +14,44 @@ class Git(object):
 			error_message('Git not installed!')
 
 	def add(self, host_name):
-		# folder = '/root/repositories'
-		# external_ip = '192.168.253.1'
-		# cd %(folder)s && mkdir %(host_name)s.git && git init --bare
-		# cd project_dir && git init && git remote add origin repository && git pull origin master
-		# git remote add origin ssh://root@%(external_ip)s/%(folder)s/%(host_name)s.git
-		# git remote add origin ssh://root@127.0.0.1/%(folder)s/%(host_name)s.git
-		pass
+		data = self.getHostData(host_name)
+		website_dir = data['website_dir']
+		repository = '%s/%s.git' % (self.base.git['repositories'], host_name)
+		post_receive = '%s/hooks/post-receive' % repository
+		external_ip = self.base.git['external_ip']
+
+		if fileExists(repository):
+			error_message('File exists! Enter another hostname, or remove old repository!')
+		system_by_code('mkdir -p %s' % repository)
+
+		system_by_code('cd %s && git init --bare 1> /dev/null' % repository)
+		system_by_code('cd %s && git init 1> /dev/null && git remote add origin %s' % (website_dir, repository))
+
+		putFile(post_receive, getTemplate('git-post-receive') % data)
+		system_by_code('chmod +x %s' % post_receive)
+
+		remote = "\tgit remote add origin ssh://root@%s%s"
+		info_message('*'*100, 'blue')
+		info_message('Successful!')
+		info_message('*'*100, 'blue')
+		info_message('Use:', 'bold')
+		info_message('\t"ssh-copy-id root@%s"' % external_ip)
+		info_message('OR', 'yellow')
+		info_message('\t"ssh-copy-id root@%s" on your host.' % host_name)
+		info_message('*'*100, 'blue')
+		info_message('Add remote repository with following command:', 'white')
+		info_message(remote % (external_ip, repository))
+		info_message('OR', 'yellow')
+		info_message(remote % (host_name, repository))
+		info_message('*'*100, 'blue')
 
 	def delete(self, host_name):
-		pass
+		data = self.getHostData(host_name)
+		website_dir = data['website_dir']
+		repository = '%s/%s.git' % (self.base.git['repositories'], host_name)
+
+		if fileExists(repository):
+			system_by_code('rm -rf %s' % repository)
+		system_by_code('cd %s && git remote rm origin' % website_dir)
+
+		info_message('Successful!')
