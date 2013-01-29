f32b42c5b0b44df540acf12aaba7ef27ec2a5750
gotlium <death-metall@mail.ru>
2013-01-29 05:20:57 +0400

    changes on ahc-web runner

--- modules/web.py
+++ modules/web.py
@@ -10,20 +10,23 @@ class Web():
 
 	def __init__(self, base):
 		base.getPathAndChDir()
+		self.get_pid_cmd = "ps aux|grep [p]ython|grep [m]anage.py|grep [" \
+						"runserver]| awk '{print $2}'"
 
 	def add(self, action):
 		os.chdir('web/')
 
 		if action == 'start':
+			status = -1
 			if not fileExists('db.sqlite'):
 				os.system('/usr/bin/python manage.py syncdb')
-			os.system(
-				'/usr/bin/nohup /usr/bin/python manage.py runserver '
-				'127.0.0.1:8131 --insecure --noreload >& /var/log/ahc-web.log &'
-			)
-			info_message('Started ..')
+			if not system(self.get_pid_cmd):
+				status = os.system(
+					'/usr/bin/nohup /usr/bin/python manage.py runserver '
+					'127.0.0.1:8131 --insecure --noreload >& /var/log/ahc-web.log &'
+				)
 		else:
-			os.system("ps aux|grep [p]ython|grep [m]anage.py|grep ["
-					  "runserver]| awk '{print $2}'|xargs kill -9>&/dev/null"
+			status = os.system(
+				"%s|xargs kill -9>&/dev/null" % self.get_pid_cmd
 			)
-			info_message('Stopped ..')
+		os._exit(0 if status == 0 else -1)
